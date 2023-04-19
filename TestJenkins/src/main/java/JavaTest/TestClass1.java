8130ae091e3d707f5dcc57b4584fbbcc4883e0ef
ebin <ebin@user>
2023-04-19 20:50:03 +0530

    commiting the changes.

--- TestJenkins/src/main/java/JavaTest/TestClass1.java
+++ TestJenkins/src/main/java/JavaTest/TestClass1.java
@@ -4,9 +4,15 @@ package JavaTest;
 
 public class TestClass1 {
 
+	public static void main (String args[])
+	{
+		TestClass1 ts=new TestClass1();
+		ts.devTest();
+	}
 	public void devTest()
 	{
 		System.out.println("i am in a java clas");
 		
 	}
 }
+
